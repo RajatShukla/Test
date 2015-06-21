180ebf5f901ae054b17d5a1fbf210263f69cd02b
RajatShukla <shukla.rajat03@gmail.com>
2015-06-21 23:37:31 +0530

    Removed Summer Camp in templates, handle the error scenario in math_util module.

--- math_util/forms.py
+++ math_util/forms.py
@@ -1,20 +1,31 @@
+from nose.plugins.plugintest import blankline_separated_blocks
+
 __author__ = 'RAJAT'
 from django import forms
+from django.utils.translation import gettext_lazy as _
+
+def empty_value_validator(value):
+    if value == '':
+        raise forms.ValidationError("This field is required")
 
 class NumberForms(forms.Form):
-    input_value = forms.CharField(max_length=100, required=True, initial=100)
+    #input_value = forms.CharField(max_length=100, required=True)
+    input_value = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Integer Number',}) ,
+                                max_length=15, label=_("Input Number"), required=True,
+                                validators=[empty_value_validator])
     #opern_list = ['celing', 'factorial', 'floor', 'frexp']
     #operation = forms.ChoiceField( choices=opern_list)
-    operation_types = (('1','Celing Function'), ('2','Factorial'), ('3','Floor'),
+    operation_types = (('1','Ceiling function'), ('2','Factorial'), ('3','Floor'),
                   ('4','Regular Expression'))
-    choiceFields = forms.ChoiceField(widget=forms.RadioSelect, choices=operation_types, initial='1', required=True)
+    choiceFields = forms.ChoiceField(widget=forms.RadioSelect, choices=operation_types, required=True, label=_("Number Theoretic Operations"))
+
     class Meta:
         fields = ('Input Number',  'Operation')
 
 
 class PowLogForms(forms.Form):
-    inp_val1 = forms.CharField(max_length=100, required=True, initial=10)
-    inp_val2 = forms.CharField(max_length=100, required=True, initial=10)
+    inp_val1 = forms.CharField(max_length=100, required=True, )
+    inp_val2 = forms.CharField(max_length=100, required=True)
     operation_types = (('1', 'Power function'), ('2', 'logarithmic Function'))
     choiceFields = forms.ChoiceField(widget=forms.RadioSelect, choices=operation_types, initial='1', required=True)
     class Meta:
@@ -23,7 +34,7 @@ class PowLogForms(forms.Form):
 
 
 class TrigonmetryForms(forms.Form):
-    inp_val1 = forms.CharField(max_length=100, required=True, initial=10)
+    inp_val1 = forms.CharField(max_length=100, required=True)
     operation_types = (('1', 'arc cosine of x, in radians'), ('2', 'arc sine of x, in radians'),
                        ('3', 'arc tangent of x, in radians'), ('4', 'cosine of x radians'),
                        ('5', 'sine of x radians'), ('6', 'tangent of x radians'))
@@ -56,18 +67,8 @@ class MatrixForm(forms.Form):
                 self.fields['c_' + str(i) + '_' + str(j)] = field
 
 
-class MatrixInputForm(forms.Form):
 
-    def __init__(self, *args, **kwargs):
-        self.ncols = kwargs.pop('ncols', 1)
-        self.nrows = kwargs.pop('nrows', 1)
 
-        super(MatrixForm, self).__init__(*args, **kwargs)
-
-        for i in range(0,self.ncols):
-            for j in range(0,self.nrows):
-                field = forms.CharField(label="",max_length=2)
-                self.fields['c_' + str(i) + '_' + str(j)] = field
 
 
 
--- math_util/views.py
+++ math_util/views.py
@@ -19,16 +19,35 @@ def number_theoretic(request):
     params = ''
     contextDict = {'forms': NumberForms, 'params':params}
     if request.method == 'POST':
-        form = NumberForms(request.POST)
+        form = NumberForms(request.POST.copy())
         if form.is_valid():
+            error_list = []
             #form.save()
             input_val  = form['input_value'].value()
             oprn_val  = form['choiceFields'].value()
-            print(input_val, oprn_val)
-            params = math_calc.num_theoritic_calc(input_val, oprn_val)
+            form.data['input_value'] = input_val
+            form.data['choiceFields'] = oprn_val
+
+            if input_val == '':
+                error_list.append("Integer field is required")
+            if oprn_val == '':
+                error_list.append("Please select operation")
+            try:
+                input_val = float(input_val)
+                params = math_calc.num_theoritic_calc(input_val, oprn_val)
+            except ValueError:
+                error_list.append("Require a number")
+
+
             contextDict['params'] = params
+            contextDict['errors'] = error_list
+            contextDict['forms'] = form
             return render_to_response('math_util/number_theoretic.html',contextDict,context)
         else:
+            error_list = []
+            error_list.append("Integer field is required")
+            error_list.append("Please select operation")
+            contextDict['errors'] = error_list
             return render_to_response('math_util/number_theoretic.html',contextDict,context)
 
 
@@ -40,16 +59,43 @@ def pow_log(request):
     params = ''
     contextDict = {'forms': PowLogForms, 'params':params}
     if request.method == 'POST':
-        form = PowLogForms(request.POST)
+        form = PowLogForms(request.POST.copy())
         if form.is_valid():
             #form.save()
+            error_list = []
             input_val1  = form['inp_val1'].value()
             input_val2  = form['inp_val2'].value()
             oprn_val  = form['choiceFields'].value()
-            params = math_calc.pow_log_calc(input_val1, input_val2, oprn_val)
+            form.data['inp_val1'] = input_val1
+            form.data['inp_val2'] = input_val2
+            form.data['choiceFields'] = oprn_val
+            if input_val1 == '':
+                error_list.append("Integer field is required for input value1")
+            if input_val2 == '':
+                error_list.append("Integer field is required for input value2")
+            if oprn_val == '':
+                error_list.append("Please select operation")
+            try:
+                input_val1 = float(input_val1)
+                input_val2 = float(input_val2)
+                params = math_calc.pow_log_calc(input_val1, input_val2, oprn_val)
+            except ValueError:
+                error_list.append("Require a number")
+            
+
             contextDict['params'] = params
+            contextDict['forms'] = form
+            contextDict['errors'] = error_list
             return render_to_response('math_util/pow_log.html',contextDict,context)
         else:
+            error_list = []
+            if form['inp_val1'].value() == '':
+                error_list.append("Integer field is required for input value1")
+            if form['inp_val2'].value() == '':
+                error_list.append("Integer field is required for input value2")
+            if form['choiceFields'].value() == '':
+                error_list.append("Please select operation")
+            contextDict['errors'] = error_list
             return render_to_response('math_util/pow_log.html',contextDict,context)
     else:
         return render_to_response('math_util/pow_log.html',contextDict,context)
@@ -61,16 +107,35 @@ def trigon_fun(request):
     params = ''
     contextDict = {'forms': TrigonmetryForms, 'params':params}
     if request.method == 'POST':
-        form = TrigonmetryForms(request.POST)
+        form = TrigonmetryForms(request.POST.copy())
         if form.is_valid():
+            error_list = []
             #form.save()
             input_val1  = form['inp_val1'].value()
+            form.data['inp_val1'] = input_val1
             #input_val2  = form['inp_val2'].value()
             oprn_val  = form['choiceFields'].value()
-            params = math_calc.trigon_calc(input_val1, oprn_val)
+            form.data['choiceFields'] = oprn_val
+            if input_val1 == '':
+                error_list.append("Require a float value")
+            if oprn_val == '':
+                error_list.append("Please select a operation")
+            try:
+                params = math_calc.trigon_calc(input_val1, oprn_val)
+            except ValueError:
+                error_list.append("Kindly enter float value in their required format")
+
             contextDict['params'] = params
+            contextDict['errors'] = error_list
+            contextDict['forms'] = form
             return render_to_response('math_util/trigon_fun.html',contextDict,context)
         else:
+            error_list = []
+            if form['inp_val1'].value() == '':
+                error_list.append("Integer field is required for input value1")
+            if form['choiceFields'].value() == '':
+                error_list.append("Please select operation")
+            contextDict['errors'] = error_list
             return render_to_response('math_util/trigon_fun.html',contextDict,context)
     else:
         return render_to_response('math_util/trigon_fun.html',contextDict,context)
@@ -82,16 +147,36 @@ def hyperbolic_fun(request):
     params = ''
     contextDict = {'forms': HyperbolicForms, 'params':params}
     if request.method == 'POST':
-        form = HyperbolicForms(request.POST)
+        form = HyperbolicForms(request.POST.copy())
         if form.is_valid():
+            error_list = []
             #form.save()
             input_val1  = form['inp_val1'].value()
             #input_val2  = form['inp_val2'].value()
             oprn_val  = form['choiceFields'].value()
-            params = math_calc.hyperbolic_fun_cal(input_val1, oprn_val)
+            form.data['inp_val1'] = input_val1
+            form.data['choiceFields'] = oprn_val
+            if input_val1 == '':
+                error_list.append("Please enter the value")
+            if oprn_val == '':
+                error_list.append("Please select a operation")
+            try:
+                params = math_calc.hyperbolic_fun_cal(input_val1, oprn_val)
+            except ValueError:
+                error_list.append("Kindly enter integer or float value")
+
             contextDict['params'] = params
+            contextDict['errors'] = error_list
+            contextDict['forms'] = form
             return render_to_response('math_util/hyperbolic_fun.html',contextDict,context)
         else:
+            error_list = []
+            if form['inp_val1'].value() == '':
+                error_list.append("Please enter the value")
+            if form['choiceFields'].value() == '':
+                error_list.append("Please select a operation")
+            contextDict['errors'] = error_list
+            contextDict['forms'] = form
             return render_to_response('math_util/hyperbolic_fun.html',contextDict,context)
     else:
         return render_to_response('math_util/hyperbolic_fun.html',contextDict,context)
@@ -106,8 +191,9 @@ def matrix_fun(request):
     #return render_to_response('math_util/matrix_fun.html',context_dict,context)
     #def my_matrix(request):
     [nrows,ncols] = [3,4]
-
-    my_form = MatrixForm(nrows=nrows, ncols=ncols)
+    method = 'GET'
+    if  request.method == 'POST':
+        method = 'POST'
     return render_to_response('math_util/matrix_fun.html', RequestContext(request,
                         {'forms':my_form, "nrows":range(nrows), "ncols":range(ncols), 'rows':range(0,2,1), 'cols':range(0,2,1)}))
 
